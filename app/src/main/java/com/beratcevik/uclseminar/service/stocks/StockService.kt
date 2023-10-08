af96159bc6f2206e4dea275d9fe4858b4a86fd03
Berat Cevik <beratbarancevik@gmail.com>
2023-10-08 17:02:42 +0100

    Complete stocks service

--- app/src/main/java/com/beratcevik/uclseminar/service/stocks/StockService.kt
+++ app/src/main/java/com/beratcevik/uclseminar/service/stocks/StockService.kt
@@ -3,44 +3,85 @@ package com.beratcevik.uclseminar.service.stocks
 import android.util.Log
 import com.beratcevik.uclseminar.service.stocks.models.Stock
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 
 class StockService(
     private val db: FirebaseFirestore
 ) : StocksServiceI {
 
     override fun getStocks(completionHandler: (List<Stock>) -> Unit) {
-        db.collection("stocks").get()
-            .addOnSuccessListener { result ->
+        db.collection("stocks")
+            .orderBy("title")
+            .addSnapshotListener { snapshot, error ->
+                error?.let {
+                    Log.w(
+                        this.javaClass.name,
+                        "Error getting stocks",
+                        it
+                    )
+                }
 
-            }
-            .addOnFailureListener { exception ->
-                Log.w(
-                    this.javaClass.name,
-                    "Error getting stocks",
-                    exception
-                )
+                snapshot?.let {
+                    completionHandler.invoke(
+                        it.documents.mapNotNull { document -> document.toObject(Stock::class.java) }
+                    )
+                }
             }
     }
 
     override fun getStockDetails(stockID: String, completionHandler: (Stock) -> Unit) {
-        db.collection("stocks").document(stockID).get()
-            .addOnSuccessListener { result ->
+        db.collection("stocks").document(stockID)
+            .addSnapshotListener { snapshot, error ->
+                error?.let {
+                    Log.w(
+                        this.javaClass.name,
+                        "Error getting stock",
+                        it
+                    )
+                }
 
-            }
-            .addOnFailureListener { exception ->
-                Log.w(
-                    this.javaClass.name,
-                    "Error getting stocks",
-                    exception
-                )
+                snapshot?.let {
+                    it.toObject(Stock::class.java)?.let { stock ->
+                        completionHandler.invoke(stock)
+                    }
+                }
             }
     }
 
     override fun updateStockDetail(stockID: String, isFavorite: Boolean) {
-//        db.collection("stocks").document(stockID).set()
+        db.collection("stocks").document(stockID).set(
+            mapOf("isFavorite" to isFavorite),
+            SetOptions.merge()
+        )
     }
 
     override fun uploadStocks() {
-//        db.collection("stocks").add()
+        val stocks = listOf(
+            Stock(
+                id = "1",
+                title = "Tesla",
+                symbol = "TSLA",
+                price = 120.6,
+                logoUrl = "https://oceansquare.com/wp-content/uploads/2018/04/tesla-logo-500.jpg",
+                isFavorite = false
+            ),
+            Stock(
+                id = "2",
+                title = "Apple",
+                symbol = "AAPL",
+                price = 120.6,
+                logoUrl = "https://i.pinimg.com/474x/b0/d2/6e/b0d26e8122dffa8a51081f7f814581d7.jpg",
+                isFavorite = false
+            ),
+            Stock(
+                id = "3",
+                title = "Meta",
+                symbol = "META",
+                price = 120.6,
+                logoUrl = "https://img.freepik.com/premium-vector/meta-company-logo_265339-667.jpg",
+                isFavorite = false
+            )
+        )
+        stocks.forEach { db.collection("stocks").add(it) }
     }
 }
